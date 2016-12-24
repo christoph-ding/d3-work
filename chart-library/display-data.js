38a994a83230eff58faa5dc674d9910506ae7363
Pokegear <christoph.ding@gmail.com>
2016-12-24 17:19:55 -0500

    Add function stubs for creating a bar chart.

--- chart-library/display-data.js
+++ chart-library/display-data.js
@@ -3,6 +3,32 @@ function displayData(data) {
   console.log('displaying data: ...');
   console.log(data);
 
-  var canvas = d3.select(".chart");
-      
+  var canvas = d3.select(".chart")
+                 .selectAll()
+                 .data(data)
+                 .enter()
+                 .append("p")
+                 .style("width", function(d) { return d.age * 10 + "px"; })
+                 .style("height", 100 + 'px')                 
+                 .style("background-color", "blue");
+
+  var bars;
+  var axis;
+  var title;
+
+  var makeAxis = function(chartElement, data) {
+
+  }
+
+  var addBars = function(chartElement, data) {
+
+  }
+
+  var consumeData = function(dataSource) {
+
+  }
+
+  var determineAxisBounds = function(data, ticks) {
+
+  }
 }
