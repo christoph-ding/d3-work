c07cb82b29ac5eda6be694c70e872d5a1e480e9b
Pokegear <christoph.ding@gmail.com>
2017-01-04 18:07:01 -0500

    Add a chart svg and an axis.

--- chart-library/display-data.js
+++ chart-library/display-data.js
@@ -1,13 +1,17 @@
 function displayData(data) {
   // for testing and debugging
   console.log('displaying data: ...');
-  console.log(data);
 
   var widthCanvas = 350;
   var heightCanvas = 350;
 
-  var canvas = d3.select(".chart")
+  var canvas = d3.select(".chart")                 
+                 .append("svg")
+                 .attr("width", 1000)
+                 .attr("height", 500)
+                 .style("background-color", "red")
 
+  // determine scales for both axes
   var maxDomainX = d3.max(data, function(d) {
     return d[0];
   });
@@ -18,16 +22,18 @@ function displayData(data) {
 
   var xScale = d3.scale.linear()
                        .domain([0, maxDomainX])
-                       .range([0, widthCanvas])
+                       .range([0, widthCanvas]);
 
   var yScale = d3.scale.linear()
                        .domain([0, maxDomainY])
-                       .range([0, heightCanvas])
+                       .range([0, heightCanvas]);
 
-  // var xAxis = d3.svg.axis()
-  //       .scale(xScale)
+  // axes
+  var xAxis = d3.svg.axis()
+                    .scale(xScale)
+                    .orient("bottom");
 
   // canvas.append("g")
-  //       .call(xAxis)
+  //       .call(xAxis);
 
 }
